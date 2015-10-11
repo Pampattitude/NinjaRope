b6bb2883f9b9b27e01c7aa3918a4eb4d479180fb
Pampattitude <guichan@hotmail.fr>
2015-10-11 17:44:37 +0200

    replaced NinjaRope collider system with raycasting
    Problem now: the NinjaRopePlayerController gets twitchy when the hook
    hits a wall, making him warp quickly down the rope, occasionally making
    the rope cling to an obstacle the player would touch. Might also mess
    with the velocity

--- NinjaRope.cs
+++ NinjaRope.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+#define DEBUG_NINJA_ROPE
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 [RequireComponent(typeof(LineRenderer))]
 public class NinjaRope: MonoBehaviour {
+    private static int          criticalIterationCount = 1000; // Debug, useful with DEBUG_NINJA_ROPE
+
     private Rigidbody2D         rigidbody_;
     private LineRenderer        lineRenderer_;
     private BoxCollider2D       collider_;
@@ -26,11 +30,13 @@ public class NinjaRope: MonoBehaviour {
 
     public float                ropeWidth = 0.12f; // The width of the rope
     public float                ropeLength = 3f; // The length
-    private float               minDistanceBetweenPoints_ = 0.1f;
+
+    public float                raycastStep = 10f;
+
+    public LayerMask            collisionMask;
 
     protected void      Start() {
         this.initRigidbody_();
-        this.initCollider_();
 
         this.lineRenderer_ = gameObject.GetComponent<LineRenderer>();
         this.lineRenderer_.SetWidth(this.ropeWidth, this.ropeWidth);
@@ -41,19 +47,39 @@ public class NinjaRope: MonoBehaviour {
         this.updateJoint_();
     }
 	
-	protected void      FixedUpdate () {
+	protected void      Update() {
 	    this.baseAnchor = new RopeAnchor(this.baseEntity.transform.position, 0f);
 	    this.targetAnchor = new RopeAnchor(this.endEntity.transform.position, 0f);
 
         this.popAnchors_();
-        this.updateCollider_();
-        this.updateRendering_();
+        this.updateRaycastCollision_();
         this.updateJoint_();
+        this.updateRendering_();
 	}
 
 
+#if DEBUG_NINJA_ROPE
+    void    OnDrawGizmos() {
+        float   opacity = 2f / 3f;
+        Color   baseColor = Gizmos.color;
+
+        Gizmos.color = new Color(0f, 0.5f, 1f, opacity);
+        for (int i = 1 ; this.anchors_.Count - 1 > i ; ++i) {
+            Gizmos.DrawSphere(this.anchors_[i].anchor, this.ropeWidth);
+        }
+
+        Gizmos.color = new Color(1f, 0.5f, 0f, opacity);
+        Gizmos.DrawSphere(this.baseAnchor.anchor, this.ropeWidth);
+        Gizmos.color = new Color(1f, 0f, 0.5f, opacity);
+        Gizmos.DrawSphere(this.targetAnchor.anchor, this.ropeWidth);
+
+        Gizmos.color = baseColor;
+    }
+#endif
+
     // Initialization
     private void    initRigidbody_() {
+        // Rigidbody is necessary for the joint collision to work
         this.rigidbody_ = gameObject.GetComponent<Rigidbody2D>();
         if (!this.rigidbody_)
             this.rigidbody_ = gameObject.AddComponent<Rigidbody2D>();
@@ -81,6 +107,50 @@ public class NinjaRope: MonoBehaviour {
 
 
     // Update
+    private void    updateRaycastCollision_() {
+        int         loopCount = 0,
+                    realLoopCount = 0;
+        Vector3     prevPos = this.lastAnchor.anchor;
+
+        do {
+            Vector3     linecastTarget = prevPos + (this.targetAnchor.anchor - this.lastAnchor.anchor) / this.raycastStep;
+            RaycastHit2D    rch = Physics2D.Linecast(prevPos, linecastTarget, this.collisionMask);
+            if (!rch || 0.0001f >= Vector3.Distance(prevPos, rch.point))
+                rch = Physics2D.Linecast(linecastTarget, prevPos, this.collisionMask);
+
+            if (rch && 0.0001f < Vector3.Distance(prevPos, rch.point)) {
+                Vector3     point = rch.point;
+
+                // We get the angle between the latest segment and the anchor-middle of platform
+                float   angle = Mathf.DeltaAngle(NinjaRope.pointsToAngle(point - this.lastAnchor.anchor, point - rch.collider.gameObject.transform.position), NinjaRope.pointsToAngle(point, this.lastAnchor.anchor));
+                float   side = 0f >= angle ? -1f : 1f;
+
+                // Finally, we add the anchor to the list
+                this.addAnchor_(new RopeAnchor(point, side));
+
+                prevPos = point;
+                loopCount = 0;
+            }
+            else {
+                prevPos += (this.targetAnchor.anchor - this.lastAnchor.anchor) / this.raycastStep; // We try to move the previous position forward a bit
+                // Note that it means the collision detection gets less and less accurate the further we get to the target anchor
+                ++loopCount;
+            }
+
+            ++realLoopCount;
+        } while (Mathf.Sign(this.lastAnchor.anchor.x - this.targetAnchor.anchor.x) == Mathf.Sign(prevPos.x - this.targetAnchor.anchor.x) &&
+                 Mathf.Sign(this.lastAnchor.anchor.y - this.targetAnchor.anchor.y) == Mathf.Sign(prevPos.y - this.targetAnchor.anchor.y)
+#if DEBUG_NINJA_ROPE
+                 && NinjaRope.criticalIterationCount > realLoopCount
+#endif
+        );
+
+#if DEBUG_NINJA_ROPE
+        if (NinjaRope.criticalIterationCount == realLoopCount)
+            Debug.Log("NinjaRope: Critical iteration count (" + NinjaRope.criticalIterationCount + ", loop count was " + loopCount + ") attained, please contact the creator of the script!");
+#endif
+    }
+
     private void    popAnchors_() {
         bool        goOn = true;
 
@@ -94,7 +164,7 @@ public class NinjaRope: MonoBehaviour {
                         act = this.targetAnchor.anchor - this.lastAnchor.anchor;
             Vector3     prevPerpendicular = Quaternion.Euler(0f, 0f, 90f * this.lastAnchor.side) * prev;
 
-            if (0f <= Vector3.Dot(prevPerpendicular, act)) {
+            if (0f < Vector3.Dot(prevPerpendicular, act)) {
                 this.lastAnchor = this.targetAnchor;
                 this.anchors_.RemoveAt(this.anchors_.Count - 1);
 
@@ -118,12 +188,6 @@ public class NinjaRope: MonoBehaviour {
                 goOn = true;
             }
         }
-    }
-
-    private void    updateCollider_(bool direct = true) {
-        this.collider_.size = new Vector2(this.ropeWidth / 2f, Vector2.Distance(this.lastAnchor.anchor, this.targetAnchor.anchor));
-        gameObject.transform.position = (this.lastAnchor.anchor + this.targetAnchor.anchor) / 2f;
-        gameObject.transform.eulerAngles = new Vector3(0f, 0f, NinjaRope.pointsToAngle(this.lastAnchor.anchor, this.targetAnchor.anchor));
     }
 
     private void    updateRendering_() {
@@ -164,60 +228,10 @@ public class NinjaRope: MonoBehaviour {
     // !Update
 
 
-    // Listeners
-    private class CollisionPointDistanceFromSort { // Sort functor (not really a functor, actually)
-        public Vector3  basePos;
-        public CollisionPointDistanceFromSort(Vector3 v) { this.basePos = v; }
-        public int      compare(ContactPoint2D c1, ContactPoint2D c2) {
-            float       diff = Vector3.Distance(this.basePos, c1.point) - Vector3.Distance(this.basePos, c2.point);
-            if (0 > diff) return -1; else if (0 < diff) return 1; return 0;
-        }
-    }
-    protected void  OnCollisionStay2D(Collision2D c) {
-        // We get the nearest vertex of the ground
-        System.Array.Sort(c.contacts, (new CollisionPointDistanceFromSort(this.lastAnchor.anchor)).compare);
-        foreach (ContactPoint2D con in c.contacts) {
-            Vector3     nearestVertex = NinjaRope.nearestVertexTo(c.gameObject, con.point);
-
-            // Then, we make sure we're not too close to the previous point (else, since this is "on stay", new points would spawn like hell)
-            Vector3 padd = nearestVertex - c.gameObject.transform.position;
-            padd.Normalize();
-            Vector3 finalNearestVertex = nearestVertex + padd * this.ropeWidth / 2f;
-
-            if (this.minDistanceBetweenPoints_ < Vector3.Distance(this.lastAnchor.anchor, finalNearestVertex)) {
-                // We get the angle between the latest segment and the anchor-middle of platform
-                float   angle = Mathf.DeltaAngle(NinjaRope.pointsToAngle(nearestVertex - this.lastAnchor.anchor, nearestVertex - c.gameObject.transform.position), NinjaRope.pointsToAngle(nearestVertex, this.lastAnchor.anchor));
-                float   side = 0f >= angle ? -1f : 1f;
-
-                // Finally, we add the anchor to the list
-                this.addAnchor_(new RopeAnchor(finalNearestVertex, side));
-            }
-        }
-    }
-    // !Listeners
-
-
     // Utils
     private static float    pointsToAngle(Vector2 anc1, Vector2 anc2) {
         // Returns the angle between both vectors; note that the angle is NOT between -180 and 180
         return (Mathf.Atan2(anc2.y - anc1.y, anc2.x - anc1.x) * Mathf.Rad2Deg - 90f);
-    }
-
-    private static Vector3  nearestVertexTo(GameObject go, Vector3 point, float z = 0f) {
-        // Get the nearest vertex, on a polygon collider, to the `point` given
-        Vector3     act = new Vector3(1000f, 1000f, 1000f);
-        bool        found = false;
-
-        foreach (Vector3 v in go.GetComponent<PolygonCollider2D>().points) {
-            if (Vector3.Distance(point, act) > Vector3.Distance(point, go.transform.TransformPoint(v))) {
-                act = go.transform.TransformPoint(v); act.z = 0f;
-                found = true;
-            }
-        }
-
-        if (!found)
-            return point;
-        return new Vector3(act.x, act.y, z);
     }
 
     
--- NinjaRopePlayerController.cs
+++ NinjaRopePlayerController.cs
@@ -1,17 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class NinjaRopePlayerController: MonoBehaviour {
-    private float   speed_ = 120f, ropeSpeed_ = 5f;
-    private float   minRopeLength_ = 0.1f, maxRopeLength_ = 12f;
-    private float   ropeAngle_ = 20f;
+    private float       speed_ = 120f, ropeSpeed_ = 5f;
+    private float       minRopeLength_ = 0.1f, maxRopeLength_ = 12f;
+    private float       ropeAngle_ = 20f;
 
     public GameObject   hookPrefab;
-    private GameObject  hook_;
+    private GameObject  hookInstance_;
+    private NinjaHook   hookScript_;
     private bool        isHooking_ = false;
 
     public GameObject   ropePrefab;
-    private GameObject  rope_;
+    private GameObject  ropeInstance_;
+    private NinjaRope   ropeScript_;
+
+    private Rigidbody2D rigidbody_;
+
+    void    Start() {
+        this.rigidbody_ = gameObject.GetComponent<Rigidbody2D>();
+    }
 
     void    Update() {
         if (!this.isHooking_) {
@@ -23,8 +32,6 @@ public class NinjaRopePlayerController: MonoBehaviour {
         }
         else {
             // Hook is either launched or hooked
-            NinjaRope   rope = this.rope_.GetComponent<NinjaRope>();
-        
             // No input, remove the hook
             if (Input.GetKeyUp(KeyCode.A) ||
                 Input.GetKeyUp(KeyCode.D)) {
@@ -32,14 +39,14 @@ public class NinjaRopePlayerController: MonoBehaviour {
                 return ;
             }
 
-            if (!this.hook_.GetComponent<NinjaHook>().isHooked()) {
+            if (!this.hookScript_.isHooked()) {
                 // The hook is launched, but not yet hooked
-                if (this.maxRopeLength_ < Vector3.Distance(this.hook_.transform.position, gameObject.transform.position)) // Remove hook if it took too long to find a spot to hook on
+                if (this.maxRopeLength_ < Vector3.Distance(this.hookInstance_.transform.position, gameObject.transform.position)) // Remove hook if it took too long to find a spot to hook on
                     this.removeHook_();
             }
             else {
                 // The hook is hooked on a surface
-                Vector3     upVector = (rope.lastAnchor.anchor - gameObject.transform.position).normalized,
+                Vector3     upVector = (this.ropeScript_.lastAnchor.anchor - gameObject.transform.position).normalized,
                             rightVector = (Quaternion.Euler(0f, 0f, -90f) * upVector).normalized;
                 Vector3 posDelta = Vector3.zero;
 
@@ -50,47 +57,49 @@ public class NinjaRopePlayerController: MonoBehaviour {
                     posDelta += rightVector * this.speed_ * Time.fixedDeltaTime;
 
                 if (0.0001f < posDelta.magnitude)
-                    gameObject.GetComponent<Rigidbody2D>().AddForce(posDelta);
+                    this.rigidbody_.AddForce(posDelta);
 
                 // Move up or down the rope
 	            if (Input.GetKey(KeyCode.W))
-                    rope.ropeLength -= this.ropeSpeed_ * Time.fixedDeltaTime;
+                    this.ropeScript_.ropeLength -= this.ropeSpeed_ * Time.fixedDeltaTime;
 	            if (Input.GetKey(KeyCode.S))
-                    rope.ropeLength += this.ropeSpeed_ * Time.fixedDeltaTime;
+                    this.ropeScript_.ropeLength += this.ropeSpeed_ * Time.fixedDeltaTime;
 
-                if (this.minRopeLength_ > rope.ropeLength)
-                    rope.ropeLength = this.minRopeLength_;
-                if (this.maxRopeLength_ < rope.ropeLength)
-                    rope.ropeLength = this.maxRopeLength_;
+                if (this.minRopeLength_ > this.ropeScript_.ropeLength)
+                    this.ropeScript_.ropeLength = this.minRopeLength_;
+                if (this.maxRopeLength_ < this.ropeScript_.ropeLength)
+                    this.ropeScript_.ropeLength = this.maxRopeLength_;
             }
         }
     }
 
     private void    launchHook_(Vector3 spd) {
         // Instantiate hook, and make the player the hook parent to ensure the hook position is affected by the player (TMP: DOES NOT WORK)
-        this.hook_ = Object.Instantiate(this.hookPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
-        this.hook_.GetComponent<NinjaHook>().direction = spd;
+        this.hookInstance_ = Object.Instantiate(this.hookPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
+        this.hookScript_ = this.hookInstance_.GetComponent<NinjaHook>();
+        this.hookScript_.direction = spd;
 
-        this.rope_ = Object.Instantiate<GameObject>(this.ropePrefab);
-        this.rope_.GetComponent<NinjaRope>().baseEntity = this.hook_;
-        this.rope_.GetComponent<NinjaRope>().endEntity = gameObject;
-        this.rope_.GetComponent<NinjaRope>().removeLength(); // Remove the max length of the rope
+        this.ropeInstance_ = Object.Instantiate<GameObject>(this.ropePrefab);
+        this.ropeScript_ = this.ropeInstance_.GetComponent<NinjaRope>();
+        this.ropeScript_.baseEntity = this.hookInstance_;
+        this.ropeScript_.endEntity = gameObject;
+        this.ropeScript_.removeLength(); // Remove the max length of the rope
 
-        this.hook_.GetComponent<NinjaHook>().onHooked += this.onHooked;
+        this.hookScript_.onHooked += this.onHooked;
 
         this.isHooking_ = true;
     }
 
     private void    removeHook_() {
         // Destroy both the hook and the rope (duh!)
-        Destroy(this.hook_);
-        Destroy(this.rope_);
+        Destroy(this.ropeInstance_);
+        Destroy(this.hookInstance_);
 
         this.isHooking_ = false;
     }
 
     public void     onHooked() {
         // Reset the max length of the rope
-        this.rope_.GetComponent<NinjaRope>().setLength(this.rope_.GetComponent<NinjaRope>().getRopeLength() + gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * Time.deltaTime);
+        this.ropeScript_.setLength(this.ropeScript_.getRopeLength());
     }
 }
